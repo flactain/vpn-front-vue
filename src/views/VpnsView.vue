fbc0903061a99f63fc23cb3d0294bbf91fd89eb6
moriyamato <flactly@gmail.com>
2025-08-01 14:03:09 +0900

    fix

--- src/views/VpnsView.vue
+++ src/views/VpnsView.vue
@@ -21,6 +21,7 @@ const privateIp = ref('')
 const subnetMask = ref('10.100.0.0/24')
 const terminalName = ref('')
 const os = ref('')
+const step = ref(1)
 
 const createNew = () => {
   selectedClient.value = ''
@@ -49,81 +50,106 @@ const selectClient = computed(() => {
                 </v-btn>
               </template>
               <v-card>
-                <v-container>
-                  <v-card-title>Join this VPN!</v-card-title>
-                  <v-card-subtitle>{{ vpnId }}</v-card-subtitle>
-                  <v-card-text>
-                    <v-form>
-                      <v-autocomplete
-                        ref="autocompleteRef"
-                        v-model="selectedClient"
-                        label="Client"
-                        :items="clients"
-                        prepend-inner-icon="mdi-monitor"
-                        variant="underlined"
-                        density="comfortable"
-                      >
-                        <template v-slot:append-item>
-                          <v-divider></v-divider>
-                          <v-list-item
-                            prepend-icon="mdi-plus"
-                            @click="createNew"
-                            class="text-primary"
-                          >
-                            <v-list-item-title>Create new client.</v-list-item-title>
-                          </v-list-item>
+                <v-card-title class="d-flex justify-space-between align-center">
+                  <div class="text-h5">Join this VPN!</div>
+                  <v-btn icon="mdi-close" variant="text" @click="dialog = false"></v-btn>
+                </v-card-title>
+                <v-card-subtitle>{{ vpnId }}</v-card-subtitle>
+                <v-window v-model="step">
+                  <v-window-item :value="1">
+                    <v-card-text>
+                      <v-form>
+                        <v-autocomplete
+                          ref="autocompleteRef"
+                          v-model="selectedClient"
+                          label="Client"
+                          :items="clients"
+                          prepend-inner-icon="mdi-monitor"
+                          variant="underlined"
+                          density="comfortable"
+                        >
+                          <template v-slot:append-item>
+                            <v-divider></v-divider>
+                            <v-list-item
+                              prepend-icon="mdi-plus"
+                              @click="createNew"
+                              class="text-primary"
+                            >
+                              <v-list-item-title>Create new client.</v-list-item-title>
+                            </v-list-item>
+                          </template>
+                        </v-autocomplete>
+
+                        <template v-if="selectClient">
+                          <v-text-field
+                            v-model="terminalName"
+                            label="terminal name"
+                            prepend-inner-icon="mdi-tag-text-outline"
+                            hint="Recomend format: ${your-name}-${terminal-type}-${OS}"
+                            placeholder="moriyamato-nuc-ubuntu"
+                            variant="underlined"
+                          ></v-text-field>
+
+                          <v-text-field
+                            v-model="os"
+                            label="OS"
+                            prepend-inner-icon="mdi-cpu-64-bit"
+                            hint="OS name"
+                            placeholder="Ubuntu 22.04"
+                            variant="underlined"
+                          ></v-text-field>
                         </template>
-                      </v-autocomplete>
-
-                      <template v-if="selectClient">
-                        <v-text-field
-                          v-model="terminalName"
-                          label="terminal name"
-                          prepend-inner-icon="mdi-tag-text-outline"
-                          hint="Recomend format: ${your-name}-${terminal-type}-${OS}"
-                          placeholder="moriyamato-nuc-ubuntu"
-                          variant="underlined"
-                        ></v-text-field>
 
                         <v-text-field
-                          v-model="os"
-                          label="OS"
-                          prepend-inner-icon="mdi-cpu-64-bit"
-                          hint="OS name"
-                          placeholder="Ubuntu 22.04"
+                          v-model="privateIp"
+                          label="Private IP"
+                          prepend-inner-icon="mdi-ip-network-outline"
+                          :hint="'Subnet CIDR:   ' + subnetMask"
+                          persistent-hint
+                          placeholder="10.100.10.1"
                           variant="underlined"
                         ></v-text-field>
-                      </template>
-
+                      </v-form>
+                    </v-card-text>
+                  </v-window-item>
+                  <v-window-item :value="2">
+                    <v-card-text>
                       <v-text-field
-                        v-model="privateIp"
-                        label="Private IP"
-                        prepend-inner-icon="mdi-ip-network-outline"
-                        :hint="'Subnet CIDR:   ' + subnetMask"
+                        v-model="publicKey"
+                        label="your public key"
+                        prepend-inner-icon="mdi-key-variant"
                         persistent-hint
-                        placeholder="10.100.10.1"
+                        placeholder="s7Pk0WuD38StoAC3t+fz5pWvkG5TEFsJGA68eba+v1c="
                         variant="underlined"
                       ></v-text-field>
-
-                      <v-row> </v-row>
-                    </v-form>
-                  </v-card-text>
-                </v-container>
+                    </v-card-text>
+                  </v-window-item>
+                </v-window>
                 <v-divider></v-divider>
                 <v-card-actions>
-                  <v-btn prepend-icon="mdi-eraser" text="CLEAR" variant="elevated"></v-btn>
-                  <v-spacer></v-spacer>
                   <v-btn
-                    prepend-icon="mdi-close-thick"
-                    text="CANCEL"
-                    color="error"
-                    variant="elevated"
+                    v-if="step !== 1"
+                    prepend-icon="mdi-arrow-left"
+                    text="BACK"
+                    variant="text"
+                    @click="step--"
                   ></v-btn>
+                  <v-spacer></v-spacer>
+                  <!-- <v-btn prepend-icon="mdi-eraser" text="CLEAR" variant="elevated"></v-btn> -->
                   <v-btn
+                    v-if="step !== 2"
                     append-icon="mdi-arrow-right"
                     text="NEXT"
                     color="primary"
-                    variant="elevated"
+                    variant="flat"
+                    @click="step++"
+                  ></v-btn>
+                  <v-btn
+                    v-if="step === 2"
+                    append-icon="mdi-send"
+                    text="submit"
+                    color="orange-darken-1"
+                    variant="flat"
                   ></v-btn>
                 </v-card-actions>
               </v-card>
